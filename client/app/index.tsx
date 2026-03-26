17ae61d6a351e4b371d197e06720b1ccf05ed20a
Tom Evans <tomevans1517@gmail.com>
2026-03-26 20:57:30 +0000

    Created new expo project with Nativewind

---
+++ client/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View} from 'react-native';
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-100">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to ReadCeipt!
+      </Text>
+    </View>
+  );
+}
