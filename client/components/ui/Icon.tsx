c5a98c5d35832beecc9e0d84d6e412bfcc2ee601
Tom Evans <tomevans1517@gmail.com>
2026-03-27 15:57:36 +0000

    Added icon component wrapping feather icons to use tailwind colours

---
+++ client/components/ui/Icon.tsx
@@ -0,0 +1,34 @@
+import Feather from '@expo/vector-icons/Feather';
+import { cssInterop } from 'nativewind';
+
+cssInterop(Feather, {
+  className: {
+    target: 'style',
+    nativeStyleToProp: { color: true },
+  },
+});
+
+const ICON_SIZES = {
+  sm: 16,
+  default: 20,
+  lg: 24,
+  xl: 32,
+};
+
+
+type IconName = React.ComponentProps<typeof Feather>['name'];
+
+interface IconProps {
+  name: IconName;
+  size?: keyof typeof ICON_SIZES;
+  className?: string; }
+
+export function Icon({ name, size = 'default', className }: IconProps) {
+  return (
+    <Feather 
+      name={name} 
+      size={ICON_SIZES[size]} 
+      className={className} 
+    />
+  );
+}
