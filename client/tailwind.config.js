515724fbf1f5d4f942ee1d90c17240d332a35b2a
Tom Evans <tomevans1517@gmail.com>
2026-03-31 20:49:57 +0100

    Added prettier format script to package.json and ran it

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,26 +5,25 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['InclusiveSans_400Regular', 'sans-serif'],
-        'sans-medium': ['InclusiveSans_500Medium', 'sans-serif'],
-        'sans-semibold': ['InclusiveSans_600SemiBold', 'sans-serif'],
-        'sans-bold': ['InclusiveSans_700Bold', 'sans-serif'],
+        sans: ["InclusiveSans_400Regular", "sans-serif"],
+        "sans-medium": ["InclusiveSans_500Medium", "sans-serif"],
+        "sans-semibold": ["InclusiveSans_600SemiBold", "sans-serif"],
+        "sans-bold": ["InclusiveSans_700Bold", "sans-serif"],
       },
       colors: {
-        background: 'rgb(var(--background) / <alpha-value>)',
-        headline: 'rgb(var(--headline) / <alpha-value>)',
-        body: 'rgb(var(--body) / <alpha-value>)',
-        muted: 'rgb(var(--muted) / <alpha-value>)',
+        background: "rgb(var(--background) / <alpha-value>)",
+        headline: "rgb(var(--headline) / <alpha-value>)",
+        body: "rgb(var(--body) / <alpha-value>)",
+        muted: "rgb(var(--muted) / <alpha-value>)",
         primary: {
           DEFAULT: "rgb(var(--primary) / <alpha-value>)",
-          foreground: "rgb(var(--primary-foreground) / <alpha-value>)", 
+          foreground: "rgb(var(--primary-foreground) / <alpha-value>)",
         },
-        secondary: 'rgb(var(--secondary) / <alpha-value>)',
-        tertiary: 'rgb(var(--tertiary) / <alpha-value>)',
-        destructive: 'rgb(var(--destructive) / <alpha-value>)',
+        secondary: "rgb(var(--secondary) / <alpha-value>)",
+        tertiary: "rgb(var(--tertiary) / <alpha-value>)",
+        destructive: "rgb(var(--destructive) / <alpha-value>)",
       },
     },
   },
   plugins: [],
 };
-
