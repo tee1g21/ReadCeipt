277333564c9acf67ec016788920d59cc8094ab7d
Tom Evans <tomevans1517@gmail.com>
2026-04-01 19:11:42 +0100

    History base screen

---
+++ client/app/(tabs)/history.tsx
@@ -0,0 +1,10 @@
+import { AppText, Button, Screen } from "@/components/ui";
+
+export default function History() {
+  return (
+    <Screen className="items-center justify-center">
+      <AppText variant="h2">We got history baby 0!</AppText>
+      <Button />
+    </Screen>
+  );
+}
