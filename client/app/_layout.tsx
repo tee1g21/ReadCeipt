85aae745f00f434c6d4f845fe23356ab3e70c81f
Tom Evans <tomevans1517@gmail.com>
2026-04-06 17:19:47 +0100

    Ensured app launches to Dashboard

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout() {
 
   return (
     <Stack
+      initialRouteName="(tabs)"
       screenOptions={{
         headerShown: false,
         animation: "default",
