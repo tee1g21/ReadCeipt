b39e3b08e2697ae6866d6901c431671036427d8f
Tom Evans <tomevans1517@gmail.com>
2026-04-07 11:09:15 +0100

    Fixed Dashboard circular import

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Dashboard } from "@/components/dashboard";
+import { Dashboard } from "@/components/dashboard/Dashboard";
 
 export default function Index() {
   return <Dashboard />;
--- client/components/dashboard/index.ts
+++ client/components/dashboard/index.ts
@@ -1,5 +1,4 @@
 export { CurrentMonthSpending } from "./CurrentMonthSpending";
-export { Dashboard } from "./Dashboard";
 export { MonthlyBreakdown } from "./MonthlyBreakdown";
 export { NumberScanned } from "./NumberScanned";
 export { RecentActivity } from "./RecentActivity";
