b8c503a04be9e66a8fc2d66db04d14bc11849fd7
Tom Evans <tomevans1517@gmail.com>
2026-04-17 17:36:37 +0100

    Created date formatter to format unix date timestamps

---
+++ client/lib/dateFormatter.ts
@@ -0,0 +1,22 @@
+import { isToday, isYesterday, isThisWeek, format } from "date-fns";
+
+export function getSectionTitle(timestamp: number): string {
+  const date = new Date(timestamp);
+
+  if (isToday(date)) return "Today";
+  if (isYesterday(date)) return "Yesterday";
+  if (isThisWeek(date, { weekStartsOn: 1 })) return "This Week";
+
+  return format(date, "MMM yyyy");
+}
+
+export function formatReceiptThumbnailTimeStamp(timestamp: number): string {
+  const date = new Date(timestamp);
+  const timeString = format(date, "HH:mm a");
+
+  if (isToday(date)) return `Today • ${timeString}`;
+  if (isYesterday(date)) return `Yesterday • ${timeString}`;
+
+  const dayString = format(date, "MMM dd");
+  return `${dayString} • ${timeString}`;
+}
