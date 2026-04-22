71eaebe706a26e3dcc1b70d29c8bc53a4462fb58
Tom Evans <tomevans1517@gmail.com>
2026-04-22 23:20:36 +0100

    Added receiptDetailScreen timestamp format to dateFormatter

--- client/lib/dateFormatter.ts
+++ client/lib/dateFormatter.ts
@@ -7,7 +7,7 @@ export function getSectionTitle(timestamp: number): string {
   if (isYesterday(date)) return "Yesterday";
   if (isThisWeek(date, { weekStartsOn: 1 })) return "This Week";
 
-  return format(date, "MMM yyyy");
+  return format(date, "MMMM yyyy");
 }
 
 export function formatReceiptThumbnailTimeStamp(timestamp: number): string {
@@ -18,5 +18,16 @@ export function formatReceiptThumbnailTimeStamp(timestamp: number): string {
   if (isYesterday(date)) return `Yesterday • ${timeString}`;
 
   const dayString = format(date, "MMM dd");
+  return `${dayString} • ${timeString}`;
+}
+
+export function formatReceiptDetailTimeStamp(timestamp: number): string {
+  const date = new Date(timestamp);
+  const timeString = format(date, "HH:mm a");
+
+  if (isToday(date)) return `Today • ${timeString}`;
+  if (isYesterday(date)) return `Yesterday • ${timeString}`;
+
+  const dayString = format(date, "MMM dd, yyyy").toLocaleUpperCase();
   return `${dayString} • ${timeString}`;
 }
